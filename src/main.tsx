cc4ace16583c4611a292565f7156931d9a15cf0b
SarangVT <svdthakare@gmail.com>
2025-05-09 18:06:52 +0530

    Ready for Deployment

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import 'react-datepicker/dist/react-datepicker.css';
 import App from './App.js'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { FormProvider } from './context/FormContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
     <FormProvider>
-    <BrowserRouter>
+    <HashRouter>
     <App/>
-    </BrowserRouter>
+    </HashRouter>
     </FormProvider>
 )
